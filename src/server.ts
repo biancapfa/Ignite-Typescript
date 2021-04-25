a8b344ee9218210a19e1eefc42dde8a1885a7a74
bianca_pfr <bianca_pfr_unifei.edu.br>
2021-04-24 23:08:08 -0300

    Módulo 1,2,3

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
+import { router } from './routes';
+import swagguerFile from './swagger.json';
+
+import "./database";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swagguerFile));
+
+app.use(router);
+
+app.listen(3333, () => console.log("Server is running!"));
+
